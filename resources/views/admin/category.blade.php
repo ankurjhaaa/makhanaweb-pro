be093af6396206b9e2208b3eeded0c99124a5d1d
ankurjha2006 <kumarankur7258@gmail.com>
2025-09-08 13:52:10 +0530

    make layout category

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.layout')
+@section('title', 'Category Page')
+
+@section('content')
+<h1>tdrfyguhuj</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,5 @@ Route::get('/admin
 ', function () {
     return view('admin/layout');
 });
+
+Route::get('admin/category', [AdminController::class, 'adminCategoryPage'])->name('adminCategoryPage');
