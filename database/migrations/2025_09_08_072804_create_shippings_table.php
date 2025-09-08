007739450f2daf7e5dd385af7ed33ac558952582
ankurjha2006 <kumarankur7258@gmail.com>
2025-09-08 13:24:14 +0530

    category table corrected

--- database/migrations/2025_09_08_072804_create_shippings_table.php
+++ database/migrations/2025_09_08_072804_create_shippings_table.php
@@ -1,15 +1,16 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateShippingTable extends Migration
-{
-    public function up()
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('shipping', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
             $table->string('method_name', 100);
             $table->decimal('cost', 10, 2);
             $table->unsignedInteger('estimated_delivery_days');
@@ -18,8 +19,11 @@ class CreateShippingTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('shipping');
     }
-}
+};
