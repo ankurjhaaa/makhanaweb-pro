26b9d1d377852f44e47198aa759f5847a1180fa0
ankurjha2006 <kumarankur7258@gmail.com>
2025-09-08 14:29:29 +0530

    category add or delete sucessfully

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -22,4 +23,22 @@ class Category extends Model
     {
         return $this->hasMany(Product::class);
     }
+
+    public function setSlugAttribute($value)
+    {
+        // Agar manually slug diya gaya hai to use karo, warna name se banao
+        $slug = $value ?: Str::slug($this->attributes['name']);
+
+        $original = $slug;
+        $count = 1;
+
+        // Jab tak unique slug na mil jaye, number add karte raho
+        while (static::where('slug', $slug)->exists()) {
+            $slug = $original . '-' . $count++;
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@ Route::get('/admin
 });
 
 Route::get('admin/category', [AdminController::class, 'adminCategoryPage'])->name('adminCategoryPage');
+Route::post('admin/addCategory', [AdminController::class, 'adminCategory'])->name('addAdminCategory');
+Route::delete('admin/deleteCategory/{id}', [AdminController::class, 'deleteCategory'])->name('deleteCategory');
