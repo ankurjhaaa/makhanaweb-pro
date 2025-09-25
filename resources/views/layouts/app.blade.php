e3cec9602aa7fa55743bfd5ce23218f958261381
ankurjha2006 <kumarankur7258@gmail.com>
2025-09-25 14:50:20 +0530

    remove menifest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,11 +40,8 @@
             }
         }
     </script>
-    @if (file_exists(public_path('build/manifest.json')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
+   
         <link rel="stylesheet" href="/css/app.css">
-    @endif
     @livewireStyles
     <script>
         window.addEventListener('redirect', event => {
